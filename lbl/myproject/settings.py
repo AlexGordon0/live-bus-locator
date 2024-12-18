6c53e10bc527119ba0f64a6973f0b6aad69093e4
em23081 <em23081@bristol.ac.uk>
2024-12-18 21:57:11 +0000

    Added requirements.txt

--- lbl/myproject/settings.py
+++ lbl/myproject/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -7,8 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%5!qe79vu!i!fn!!16abud6f2i7du%96%$1*t+pu_pxbdw*)um'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
