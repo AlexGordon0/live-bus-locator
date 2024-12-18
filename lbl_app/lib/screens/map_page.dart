a909e0909a1ed2a78c7ac93c76fd7a6766450c69
em23081 <em23081@bristol.ac.uk>
2024-12-18 15:12:51 +0000

    Added map integration

---
+++ lbl_app/lib/screens/map_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Map"),
+      ),
+      body: FlutterMap(
+        options: const MapOptions(),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+          ),
+        ],
+      ),
+    );
+  }
+}
